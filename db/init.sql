36ea9871c4a090ec5b728424c5f12a70eae61d03
Zheng Lei <shanshuieast@gmail.com>
2017-06-22 14:46:22 +0800

    Grant user 'tsaruser' access database everywhere.

--- db/init.sql
+++ db/init.sql
@@ -1,4 +1,4 @@
 DROP database IF EXISTS tsar;
 CREATE database IF NOT EXISTS tsar;
-GRANT CREATE,SELECT,INSERT,UPDATE,DELETE ON tsar.* TO tsaruser@localhost IDENTIFIED BY 'tsarpw';
+GRANT CREATE,SELECT,INSERT,UPDATE,DELETE ON tsar.* TO 'tsaruser'@'%' IDENTIFIED BY 'tsarpw';
 flush privileges;
